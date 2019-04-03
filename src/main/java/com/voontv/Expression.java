96938c01b76b68082a1a3a3178fba25be3bed7a8
unknown <definevictory111@gmail.com>
2019-04-03 21:22:54 +0700

    Wrong

--- src/main/java/com/voontv/Expression.java
+++ src/main/java/com/voontv/Expression.java
@@ -13,4 +13,15 @@ public class Expression {
     public int divide(int x, int y){
         return x/y;
     }
+
+    public int max(int x, int y, int z){
+        if(x>y && x>z){
+            return x;
+        }
+
+        if(y>z){
+            return y;
+        }
+        return z;
+    }
 }
--- src/test/java/com/voontv/ExpressionTest.java
+++ src/test/java/com/voontv/ExpressionTest.java
@@ -16,8 +16,14 @@ public class ExpressionTest {
     public void test_subtract(){
         Expression expression = new Expression();
         assertEquals(expression.subtract(4, 2), 2);
-
     }
 
+    @Test
+    public void test_max(){
+        Expression expression = new Expression();
 
+        assertEquals(expression.max(1, 2, 3), 3);
+        assertEquals(expression.max(1, 8, 3), 8);
+        assertEquals(expression.max(9, 2, 3), 3);
+    }
 }
