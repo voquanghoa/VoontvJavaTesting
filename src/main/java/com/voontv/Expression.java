cd92eb0eec4680341da29473f36545f36f078e54
Võ Quang Hòa <hvquang@openwt.com>
2019-04-03 14:00:50 +0700

    Init

---
+++ src/main/java/com/voontv/Expression.java
@@ -0,0 +1,12 @@
+package com.voontv;
+
+public class Expression {
+
+    public int add(int x, int y){
+        return x + y;
+    }
+
+    public int subtract(int x, int y){
+        return x - y;
+    }
+}
---
+++ src/test/java/com/voontv/ExpressionTest.java
@@ -0,0 +1,20 @@
+package com.voontv;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ExpressionTest {
+
+    @Test
+    public void test_add(){
+        Expression expression = new Expression();
+        assertEquals(expression.add(1, 2), 3);
+    }
+
+    @Test
+    public void test_subtract(){
+        Expression expression = new Expression();
+        assertEquals(expression.subtract(1, 2), -1);
+    }
+}
