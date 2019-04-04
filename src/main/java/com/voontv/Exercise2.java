ae54512fe71c4db24a3feb10b9eef504fb12780f
voontv <voontv@gmail.com>
2019-04-04 16:23:15 +0700

    Fixed feedback exercise 2

--- src/main/java/com/voontv/Exercise2.java
+++ src/main/java/com/voontv/Exercise2.java
@@ -2,17 +2,17 @@ package com.voontv;
 
 public class Exercise2 {
 
-    public int getValueFiboInPosition (int n) {
-        if( n == 1 || n ==2) {
+    public int getValueFiboInPosition(int position) {
+        if( position == 1 || position ==2) {
             return 1;
         }
-        return getValueFiboInPosition (n -1) + getValueFiboInPosition (n-2);
+        return getValueFiboInPosition(position - 1) + getValueFiboInPosition(position - 2);
     }
 
-    public int totalFiboInRangeLimit (int n) {
+    public int totalFiboInRangeLimit(int position) {
         int total = 0;
 
-        for(int i=1; i<=n ; i++) {
+        for(int i=1; i<=position ; i++) {
             total += getValueFiboInPosition(i);
         }
         return total;
