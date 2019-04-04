fc60dd5802158c012b203e9fb7d25d0577bb8185
unknown <definevictory111@gmail.com>
2019-04-04 07:48:18 +0700

    completed exercise1 with unit test

--- src/main/java/com/voontv/Exercise1.java
+++ src/main/java/com/voontv/Exercise1.java
@@ -1,13 +1,13 @@
 package com.voontv;
 
 public class Exercise1 {
-    public boolean isCheckFirstRulesWithUsername (String username) {
+    public boolean checkFirstRulesWithUsername (String username) {
         boolean check=true;
 
         for (int i=0; i < username.length(); i++){
             char c = username.charAt(i);
-            if ((!Character.isDigit(c) && !Character.isLetter(c) && (c != '_')
-                    && !isSpecialAcceptInUsername(c))) {
+            if (!Character.isDigit(c) && !Character.isLetter(c) && (c != '_')
+                    && !isSpecialAcceptInUsername(c)) {
                 return false;
             }
 
@@ -15,7 +15,7 @@ public class Exercise1 {
         return check;
     }
 
-    public boolean isCheckSecondRulesWithUsername (String username) {
+    public boolean checkSecondRulesWithUsername (String username) {
         boolean check=true;
 
         for (int i = 0; i < username.length()-1; i++) {
@@ -23,6 +23,51 @@ public class Exercise1 {
             char nextChar = username.charAt(i+1);
             if (isSpecialAcceptInUsername(c) && isSpecialAcceptInUsername(nextChar)) {
                 return  false;
+            }
+        }
+        return check;
+    }
+
+    public boolean checkFistAndLastCharacter (String username) {
+
+        return (Character.isLowerCase (username.charAt(0)) && Character.isLowerCase(username.charAt(username.length()-1)));
+    }
+
+    public boolean checkFirstRulesWithDomain (String domain) {
+        boolean check = true;
+
+        for (int i=0; i<domain.length(); i++) {
+            char c = domain.charAt(i);
+            if(!Character.isDigit(c) && !Character.isLetter(c) && (c != '_') && (c != '.')) {
+                return false;
+            }
+        }
+        return check;
+    }
+
+    public boolean countDotsInDomain (String domain) {
+        boolean check = true;
+        int count = 0;
+
+        for ( int i = 0; i<domain.length(); i++) {
+            if (domain.charAt(i) == '.') {
+                count++;
+            }
+        }
+        if(count >3 || count <1) {
+            return false;
+        }
+        return check;
+    }
+    public boolean checkRulesPositionsDots (String domain) {
+        boolean check = true;
+
+        for (int i=0; i<domain.length()-1; i++) {
+            char c = domain.charAt(i);
+            char nextChar = domain.charAt(i+1);
+            if ((c == '.' && i == 0) || (i == domain.length()-2 && nextChar == '.')
+                    || (c == nextChar && c == '.')) {
+                return false;
             }
         }
         return check;
--- src/test/java/com/voontv/Exercise1Test.java
+++ src/test/java/com/voontv/Exercise1Test.java
@@ -7,18 +7,50 @@ import static org.junit.Assert.assertEquals;
 public class Exercise1Test {
 
     @Test
-    public void test_isCheckFirstRulesWithUsername () {
+    public void test_checkFirstRulesWithUsername () {
         Exercise1 expression = new Exercise1();
-        assertEquals(expression.isCheckFirstRulesWithUsername("maimaimottinhyeu"),true);
-        assertEquals(expression.isCheckFirstRulesWithUsername("maimaimottinhyeu_"),true);
-        assertEquals(expression.isCheckFirstRulesWithUsername("maimaimottinhyeu?"),false);
+        assertEquals(expression.checkFirstRulesWithUsername("maimaimottinhyeu"),true);
+        assertEquals(expression.checkFirstRulesWithUsername("maimaimottinhyeu_"),true);
+        assertEquals(expression.checkFirstRulesWithUsername("maimaimottinhyeu?"),false);
     }
 
     @Test
-    public void test_isCheckSecondRulesWithUsername () {
+    public void test_checkSecondRulesWithUsername () {
         Exercise1 expression = new Exercise1();
-        assertEquals(expression.isCheckSecondRulesWithUsername("maimaimottinhyeu--"),false);
-        assertEquals(expression.isCheckSecondRulesWithUsername("maimaimot.-tinhyeu_"),false);
-        assertEquals(expression.isCheckSecondRulesWithUsername("maimaimo._-ttinhyeu?"),true);
+        assertEquals(expression.checkSecondRulesWithUsername("maimaimottinhyeu--"),false);
+        assertEquals(expression.checkSecondRulesWithUsername("maimaimot.-tinhyeu_"),false);
+        assertEquals(expression.checkSecondRulesWithUsername("maimaimo._-ttinhyeu?"),true);
+    }
+
+    @Test
+    public void test_checkFistAndLastCharacter () {
+        Exercise1 exercise1 = new Exercise1 ();
+        assertEquals (exercise1.checkFistAndLastCharacter("Toi_la_toi"),false);
+        assertEquals (exercise1.checkFistAndLastCharacter("toi_la_toi"),true);
+        assertEquals (exercise1.checkFistAndLastCharacter("Toi_la_toI"),false);
+    }
+
+    @Test
+    public void test_checkFirstRulesWithDomain () {
+        Exercise1 exercise1 = new Exercise1();
+        assertEquals (exercise1.checkFirstRulesWithDomain (".com"),true);
+        assertEquals (exercise1.checkFirstRulesWithDomain ("._com"),true);
+        assertEquals (exercise1.checkFirstRulesWithDomain("-dot.com"),false);
+    }
+
+    @Test
+    public void test_countDotsInDomain () {
+        Exercise1 exercise1 = new Exercise1();
+        assertEquals (exercise1.countDotsInDomain("...domain.com"),false);
+        assertEquals (exercise1.countDotsInDomain("..domain.com"),true);
+    }
+
+    @Test
+    public void test_checkRulesPositionsDots () {
+        Exercise1 exercise1 = new Exercise1();
+        assertEquals(exercise1.checkRulesPositionsDots(".domain.com"),false);
+        assertEquals(exercise1.checkRulesPositionsDots("domain.com."),false);
+        assertEquals(exercise1.checkRulesPositionsDots("domain..com"),false);
+        assertEquals(exercise1.checkRulesPositionsDots("dom.ain.com"),true);
     }
 }
